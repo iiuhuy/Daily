7701e194539d85c574933aeccd4122ab50782e1f
AlvinMi <alvin.mi0620@gmail.com>
2019-04-18 23:52:42 +0800

    Apr.18

--- WebGL-Learning/WebGLProgramming-Guide/WebGL-canvasBasic/03HelloPoint1.js
+++ WebGL-Learning/WebGLProgramming-Guide/WebGL-canvasBasic/03HelloPoint1.js
@@ -1,14 +1,15 @@
 // HelloPoint.js
 // 顶点着色器程序
 let VSHADER_SOURCE =
+  'attribute vec4 a_Position;\n' + // 声明 Attribute 变量
   'void main() {\n' +
-  ' gl_Position = vec4(0.0, 0.5, 0.0, 1.0);\n' + // 设置坐标
+  ' gl_Position = a_Position;\n' +
   ' gl_PointSize = 10.0;\n' + // 设置尺寸 -> gl_PointSize
   '}\n';
 
 // 片元着色器程序
 let FSHADER_SOURCE =
-  'void main() {\n' + 
+  'void main() {\n' +
   ' gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);\n' + // 设置颜色
   '}\n';
 
@@ -30,8 +31,21 @@ function main() {
     return;
   }
 
+  // 获取 Attribute 变量的储存位置
+  // 第一个参数是：程序对象(program object) 包括顶点着色器和片元着色器在第八章详细介绍
+  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  console.log("a_Position Value is: " + a_Position);
+  if (a_Position < 0) {
+    console.log('Failed to get the storage location of a_Position');
+    return;
+  }
+
+  
+  // 将顶点位置传输给 Attribute 变量
+  gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
+
   // 指定清空 canvas 的颜色
-  gl.clearColor(0.5, 0.5, 0.5, 1.0);
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
   // 清空 canvas
   gl.clear(gl.COLOR_BUFFER_BIT);
