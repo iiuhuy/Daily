1906dbfe80e928433045d6b0e07eb69925150ac0
AlvinMi <alvin.mi0620@gmail.com>
2019-05-11 23:30:19 +0800

    May.11

---
+++ React-study/debug/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Table from './Table'
+import './index.css';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Table/>
+      </div>
+    );
+  }
+}
---
+++ React-study/debug/src/Table.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+export default class Table extends Component {
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Job</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Charlie</td>
+            <td>Janitor</td>
+          </tr>
+          <tr>
+            <td>Mac</td>
+            <td>Bouncer</td>
+          </tr>
+          <tr>
+            <td>Dee</td>
+            <td>Aspiring</td>
+          </tr>
+          <tr>
+            <td>Dennis</td>
+            <td>Bartender</td>
+          </tr>
+        </tbody>
+      </table> 
+    )
+  }
+}
+
