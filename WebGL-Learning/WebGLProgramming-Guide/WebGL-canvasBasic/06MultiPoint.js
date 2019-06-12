00c056f4e557ec604674d818511d4a220943f7c5
AlvinMi <alvin.mi0620@gmail.com>
2019-06-12 23:35:42 +0800

    June.12

--- WebGL-Learning/WebGLProgramming-Guide/WebGL-canvasBasic/06MultiPoint.js
+++ WebGL-Learning/WebGLProgramming-Guide/WebGL-canvasBasic/06MultiPoint.js
@@ -6,23 +6,30 @@
 // 5.清空 canvas
 // 6.绘制
 
+/**
+ * 使用缓冲区对象向顶点着色器传入多个顶点的数据
+ * 1.创建缓冲区对象  gl.createBuffer()
+ * 2.绑定缓冲区对象  gl.bindBuffer()
+ * 3.将数据写入缓冲区对象  gl.bufferData()
+ * 4.将缓冲区对象分配给一个 Attribute 变量  gl.vertexAttribPointer()
+ * 5.开启 attribute 变量   gl.enableVertexAttribArray()
+ */
+
 // 顶点着色器
 let VSHADER_SOURCE =
-  'attribute vec4 a_Position;\n' +
-  'void main() {\n' +
-  '  gl_Position = a_Position;\n' +
-  '  gl_PointSize = 10.0;\n' +
-  '}\n';
+  "attribute vec4 a_Position;\n" +
+  "void main() {\n" +
+  "  gl_Position = a_Position;\n" +
+  "  gl_PointSize = 10.0;\n" +
+  "}\n";
 
 // 片元着色器
 let FSHADER_SOURCE =
-  'void main() {\n' +
-  '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' +
-  '}\n';
+  "void main() {\n" + "  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n" + "}\n";
 
 function main() {
-  // 获取 canvas 
-  let canvas = document.getElementById('webgl');
+  // 获取 canvas
+  let canvas = document.getElementById("webgl");
 
   // 获取上下文
   let gl = getWebGLContext(canvas);
@@ -51,16 +58,14 @@ function main() {
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   // 绘制三个点
-  gl.drawArrays(gl.POINTS, 0, n); // n 是 3 
+  gl.drawArrays(gl.POINTS, 0, n); // n 是 3
 
   // 获取 a_Position 变量的存储位置
   // let a_Positi = gl.getAttribLocation(gl.program, 'a_Position');
 }
 
 function initVertexBuffers(gl) {
-  var vertices = new Float32Array([
-    0.0, 0.5, -0.5, -0.5, 0.5, -0.5
-  ]);
+  var vertices = new Float32Array([0.0, 0.5, -0.5, -0.5, 0.5, -0.5]);
 
   var n = 3;
 
@@ -76,9 +81,9 @@ function initVertexBuffers(gl) {
   // 向缓冲区对象中写入数据
   gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
-  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  var a_Position = gl.getAttribLocation(gl.program, "a_Position");
   if (a_Position < 0) {
-    console.log('获取本地存储位置失败！');
+    console.log("获取本地存储位置失败！");
     return -1;
   }
 
