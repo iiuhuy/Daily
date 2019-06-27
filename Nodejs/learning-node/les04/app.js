a96c89121c2b38646903d39677867521956d9563
AlvinMi <alvin.mi0620@gmail.com>
2019-06-27 23:41:35 +0800

    June.27

---
+++ Nodejs/learning-node/les04/app.js
@@ -0,0 +1,66 @@
+// 《使用 eventproxy 控制并发》
+// 输出 CNode (https://cnodejs.org/ ) 社区首页的所有主题的标题，链接和第一条评论，以 json 的格式。
+var eventproxy = require("eventproxy");
+var cheerio = require("cheerio");
+var superagent = require("superagent");
+
+// url 模块是 Node.js 标准库里面的
+// http://cnodejs.org/api/url.html
+var url = require("url");
+const cnodeUrl = "https://cnodejs.org/";
+
+superagent.get(cnodeUrl).end(function(err, res) {
+  if (err) {
+    return console.error(err);
+  }
+  var topicUrls = [];
+  var $ = cheerio.load(res.text);
+  // 获取首页所有的链接
+  $("#topic_list .topic_title").each(function(idx, element) {
+    var $element = $(element);
+
+    // $element.attr('href') 本来的样子是 /topic/542acd7d5d28233425538b04
+    // 用 url.resolve 来自动推断出完整 url，变成
+    // https://cnodejs.org/topic/542acd7d5d28233425538b04 的形式
+    // 看 http://nodejs.org/api/url.html#url_url_resolve_from_to 的示例
+
+    var href = url.resolve(cnodeUrl, $element.attr("href"));
+    topicUrls.push(href);
+  });
+
+  console.log(topicUrls);
+
+  var ep = new eventproxy();
+
+  ep.after("topic_html", topicUrls.length, function(topics) {
+    topics = topics.map(function(topicPair) {
+      var topicUrl = topicPair[0];
+      var topicHtml = topicPair[1];
+      var $ = cheerio.load(topicHtml);
+      return {
+        title: $(".topic_full_title")
+          .text()
+          .trim(),
+        href: topicUrl,
+        comment1: $(".reply_content")
+          .eq(0)
+          .text()
+          .trim()
+      };
+    });
+
+    console.log("final:");
+    console.log(topics);
+  });
+
+  topicUrls.forEach(function(topicUrl) {
+    superagent.get(topicUrl).end(function(err, res) {
+      console.log("fetch " + topicUrl + " successful");
+      ep.emit("topic_html", [topicUrl, res.text]);
+    });
+  });
+});
+
+// app.listen(3000, function() {
+//   console.log("app is listening at port 3000");
+// });
