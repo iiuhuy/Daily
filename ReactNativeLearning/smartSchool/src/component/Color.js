e5ce5244a0b133326e260b07316b49ea48265dd5
AlvinMi <alvin.mi0620@gmail.com>
2019-08-30 20:14:45 +0800

    rm react native learning smart

--- ReactNativeLearning/smartSchool/src/component/Color.js
+++
@@ -1,5 +0,0 @@
-export default {
-  themeColor: '#268dcd',
-  separatorColor: '#e0e0e0',
-  backgroundColor: '#f3f3f3'
-}
--- ReactNativeLearning/smartSchool/src/native_modules/SplashScreen.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-import { NativeModules } from "react-native";
-export default NativeModules.SplashScreen;
