fb12057007524c65312d3166cc1b1fd89fe3e134
AlvinMi <alvin.mi0620@gmail.com>
2019-11-22 23:55:36 +0800

    next 传递参数

--- Nodejs/nextjs-proj(Manual)/pages/a.js
+++ Nodejs/nextjs-proj(Manual)/pages/a.js
@@ -1,17 +1,27 @@
 import React, { Component, Fragment } from "react";
 import { withRouter } from "next/router";
 import Link from "next/link";
+import { resolve } from "any-promise";
 // import Comp from "../component/comp";
 // const Show = ({ router }) => <Comp>query: {router.query.id} </Comp>;
 class Show extends Component {
+  static getInitialProps = async () => {
+    const promise = new Promise(resolve => {
+      setTimeout(() => {
+        resolve({ name: "Yuhui" });
+      }, 1000);
+    });
+    return await promise;
+  };
+
   render() {
     const { router } = this.props;
-    console.log(router.query.id);
+    console.log(router.query.id, this.props.name);
     return (
       <Fragment>
-        <Link href='#aaa'>
+        <Link href="#aaa">
           <button onClick={this.goToTestB}>
-            A 页面 -> query：{router.query.id}
+            A 页面 -> query：{this.props.name}
           </button>
         </Link>
       </Fragment>
