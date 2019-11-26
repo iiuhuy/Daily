07d1828925716731405ee8208b5afecd2b08f1d2
AlvinMi <alvin.mi0620@gmail.com>
2019-11-26 23:49:24 +0800

    集成 styled-component

--- Nodejs/nextjs-proj(Manual)/pages/_document.js
+++ Nodejs/nextjs-proj(Manual)/pages/_document.js
@@ -1,13 +1,35 @@
 import Document, { Html, Main, NextScript } from "next/document";
 import Head from "next/head";
+import { Component } from "react";
+import { ServerStyleSheet } from "styled-components";
 export default class MyDocument extends Document {
   // 不必重写该方法，重写了就必须执行D ocument.getInitialProps 方法
   static getInitialProps = async ctx => {
-    // 要么不覆盖, 覆盖了至少就需要执行下面的代码
-    const pageProps = await Document.getInitialProps(ctx);
-    return {
-      ...pageProps
-    };
+    const sheet = new ServerStyleSheet();
+
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+        });
+
+      // 要么不覆盖, 覆盖了至少就需要执行下面的代码
+      const props = await Document.getInitialProps(ctx);
+      return {
+        ...props,
+        styles: (
+          <>
+            {/* props.styles 就是 next 内置的 jsx 样式 */}
+            {props.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      };
+    } finally {
+      sheet.seal();
+    }
   };
 
   render() {
--- Nodejs/nextjs-proj(Manual)/pages/a.js
+++ Nodejs/nextjs-proj(Manual)/pages/a.js
@@ -2,10 +2,14 @@ import React, { Component, Fragment } from "react";
 import { withRouter } from "next/router";
 import Head from "next/head";
 import Link from "next/link";
-import { resolve } from "any-promise";
+// import { resolve } from "any-promise";
+import styled from "styled-components";
 // const Show = ({ router }) => <Comp>query: {router.query.id} </Comp>;
-const color = "yellow";
-
+const color = "pink";
+const Title = styled.h1`
+  color: yellow;
+  font-size: 40px;
+`;
 class Show extends Component {
   static getInitialProps = async ctx => {
     const promise = new Promise(resolve => {
@@ -21,6 +25,7 @@ class Show extends Component {
     console.log(router.query.id, this.props.name);
     return (
       <Fragment>
+        <Title>这里是 Title</Title>
         <Link href="#aaa">
           <a className="link" onClick={this.goToTestB}>
             A 页面 -> query：{this.props.name}
