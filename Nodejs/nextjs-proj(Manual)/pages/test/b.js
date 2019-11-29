a8c83436504ad93179ee9dd0d0809ba3a5fd88ca
AlvinMi <alvin.mi0620@gmail.com>
2019-11-29 23:42:50 +0800

    use react hooks

--- Nodejs/nextjs-proj(Manual)/pages/test/b.js
+++ Nodejs/nextjs-proj(Manual)/pages/test/b.js
@@ -1,1 +1,45 @@
-export default () => <span>bbbb </span>;
+import { useState, useEffect } from "react";
+
+// 类的写法
+class MyCount extends React.Component {
+  state = {
+    count: 0
+  };
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({
+        count: this.state.count + 1
+      });
+    }, 1000);
+  }
+
+  componentWillMount() {
+    if (this.interval) {
+      clearInterval(this.state);
+    }
+  }
+
+  render() {
+    return <span>{this.state.count}</span>;
+  }
+}
+
+function MyCountFunc() {
+  const [count, setCount] = useState(0); // 数组 -> 结构的方式赋值
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // setCount 更新状态
+      setCount(c => c + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return <span>{count}</span>;
+}
+// export default MyCount;
+export default MyCountFunc;
