45f107c0e67a0c103d476f623ade4816d63b2896
AlvinMi <alvin.mi0620@gmail.com>
2019-12-01 23:43:11 +0800

    state hook

--- Nodejs/nextjs-proj(Manual)/pages/test/b.js
+++ Nodejs/nextjs-proj(Manual)/pages/test/b.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import react, { useState, useEffect, useReducer } from "react";
 
 // 类的写法
 class MyCount extends React.Component {
@@ -25,13 +25,35 @@ class MyCount extends React.Component {
   }
 }
 
-function MyCountFunc() {
-  const [count, setCount] = useState(0); // 数组 -> 结构的方式赋值
+// useReducer
+function countReducer(state, action) {
+  switch (action.type) {
+    case "add":
+      return state + 1;
+      // return Object.assign;
+    case "minus":
+      return state - 1;
 
+    default:
+      break;
+  }
+}
+
+// useState
+function MyCountFunc() {
+  // const [count, setCount] = useState(0); // 数组 -> 结构的方式赋值
+
+  // useReducer 写法
+  const [count, dispatchCount] = useReducer(countReducer, 10); // 第二个参数是初始值。没有初始值就为 NAN
   useEffect(() => {
     const interval = setInterval(() => {
       // setCount 更新状态
-      setCount(c => c + 1);
+      // setCount(1); // 不基于最新的值
+      // setCount(c => c + 1); // 基于最新的值(回调)
+
+      // --- useReducer
+      // dispatchCount({ type: "add" });
+      dispatchCount({ type: "minus" });
     }, 1000);
 
     return () => {
