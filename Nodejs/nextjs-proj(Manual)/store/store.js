a70f6ea616c93cde7bb983adacdf9856117a2507
AlvinMi <alvin.mi0620@gmail.com>
2019-12-08 23:35:38 +0800

    reducer 拆分

--- Nodejs/nextjs-proj(Manual)/store/store.js
+++ Nodejs/nextjs-proj(Manual)/store/store.js
@@ -1,11 +1,16 @@
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 
 const initialState = {
   count: 0
 };
 
+// reducer 状态多的情况下, 对 reducer 进行一个拆分
+const userInitialState = {
+  userName: "jokcy"
+};
+
 const ADD = "ADD";
-function reducer(state = initialState, action) {
+function countReducer(state = initialState, action) {
   console.log(state, action);
   switch (action.type) {
     case ADD:
@@ -16,11 +21,39 @@ function reducer(state = initialState, action) {
   }
 }
 
-const store = createStore(reducer, initialState);
+//
+const UPDATE_USERNAME = "UPDATE_USERNAME";
+function userReducer(state = userInitialState, action) {
+  switch (action.type) {
+    case UPDATE_USERNAME:
+      return {
+        // 如果 key 多的情况下
+        ...state,
+        username: action.name
+      };
+
+    default:
+      // break;
+      return state;
+  }
+}
+
+const allReducers = combineReducers({
+  count: countReducer,
+  user: userReducer
+});
+
+// 自动将 state 进行了模块区分 {counter: initialState}
+
+const store = createStore(allReducers, {
+  count: initialState,
+  user: userInitialState
+});
 console.log("store", store);
 console.log("getState", store.getState());
 
 store.dispatch({ type: ADD });
+store.dispatch({ type: UPDATE_USERNAME, name: "worinige" });
 
 // 监听 store 变化的 API
 store.subscribe(() => {
