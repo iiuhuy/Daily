0601dfea57670dcc4ef913cce9dad821c97e22ce
AlvinMi <alvin.mi0620@gmail.com>
2019-12-19 23:13:21 +0800

    cookie

--- Nodejs/nextjs-proj(Manual)/README.md
+++ Nodejs/nextjs-proj(Manual)/README.md
@@ -17,7 +17,37 @@ Github 认证流程:
 
 - [Authorizing OAuth Apps](https://developer.github.com/apps/building-oauth-apps/authorizing-oauth-apps/)
 
-`GET https://github.com/login/oauth/authorize` 请求这个这参数, 然后在 url 后面加上 `client_id` 参数。就能进入认证流程。
+`GET https://github.com/login/oauth/authorize` 请求这个这参数, 然后在 url 后面加上 `client_id` 参数。
+`https://github.com/login/oauth/authorize?client_id=0754fc00a257ad4c4971`
+就能进入认证流程。
 
 <img src="https://raw.githubusercontent.com/AlvinMi/2019-Pic/master/2019/20191218234104.png"/>
 
+后面再跟 repo，`https://github.com/login/oauth/authorize?client_id=0754fc00a257ad4c4971&&scope=repo`, 包含了 repo 所有的东西。[相关参数](https://developer.github.com/apps/building-oauth-apps/understanding-scopes-for-oauth-apps/#available-scopes)
+
+授权输入密码后，跳转到了 localhost:3000 并且携带了 code 参数：
+
+`http://localhost:3000/auth?code=c202cd6820ca74404930`
+
+这个 url 要跟注册 OAuth Apps 时候所填写的一样。
+
+## 请求 token
+
+- client_id、client_secret
+- Code
+- redirect_uri state
+
+模拟一个 POST 请求: `POST https://github.com/login/oauth/access_token`
+
+使用 Chrome 插件 [Rest API Testing](https://chrome.google.com/webstore/detail/talend-api-tester-free-ed/aejoelaoggembcahagimdiliamlcdmfm) 插件
+
+填写 client_id、client_secret、Code 就能拿到 token。拿到 token 之后就能去请求个人信息。
+
+`https://api.github.com/user`
+
+## OAuth Code 如何保证安全？
+
+- Code
+- client_id
+- client_secret
+- redirect_uri(需要跟注册时候的 url 一样)
--- Nodejs/nextjs-proj(Manual)/server.js
+++ Nodejs/nextjs-proj(Manual)/server.js
@@ -1,53 +1,8 @@
-// const Koa = require("koa");
-// const next = require("next");
-// const Router = require("koa-router");
-
-// const dev = process.env.NODE_ENV !== "production";
-// const app = next({ dev });
-// const router = new Router();
-// const handle = app.getRequestHandler();
-
-// // 例如 GET
-// router.get("/test/:id", ctx => {
-//   // id 是 koa-router 里面的东西. 而不是 koa 本身自带的东西
-//   // ctx.body = `<p>request /test ${ctx.params.id}</p>`; // test
-//   ctx.body = { success: true };
-//   ctx.set("Content-Type", "application/json");
-// });
-
-// // app.prepare().then(() => {
-// const server = new Koa();
-
-// // 处理异步的内容 async、koa2 async, generator -> 是 koa1 中的用法
-// server.use(async (ctx, next) => {
-//   // const path = ctx.path; // path 根据请求的路径来获取到路径
-//   // const method = ctx.method;
-//   // ctx.body = `<span>Koa is render! </br>${path} </br> 请求方法: ${method}<span>`;
-//   await next(); // 如果需要执行下面的 中间件, 那么就需要执行 await next()，否则就会一直执行着当前的 中间件
-// });
-
-// // 使用 router
-// server.use(router.routes());
-
-// // server.use(async (ctx, next) => {
-// //   ctx.body = "<span>Koa is render => 2!<span>";
-// // });
-
-// // 中间件
-// // server.use(async (ctx, next) => {
-// //   await handle(ctx.req, ctx.res);
-// //   ctx.respond = false;
-// // });
-
-// server.listen(3000, () => {
-//   console.log("koa server listening 3000: http://localhos:3000");
-// });
-// // });
-
 // =================================================================== //
 const Koa = require("koa");
 const next = require("next");
 const Router = require("koa-router");
+const session = require("koa-session");
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({
@@ -61,23 +16,71 @@ app.prepare().then(() => {
   // 实例化 Koa Server
   const server = new Koa();
   const router = new Router();
+
+  server.keys = ["yuhui develop github next app"];
+  const SESSION_CONFIG = {
+    key: "jid"
+    // store: {}
+  };
+
+  server.use(session(SESSION_CONFIG, server));
+
+  // server.use(async (ctx, next) => {
+  //   if (ctx.cookies.get("jid")) {
+  //     ctx.session = {};
+  //   }
+  //   await next();
+  // });
+
+  // server.use(async (ctx, next) => {
+  //   if (ctx.session.user) {
+  //     ctx.session.user = {
+  //       name: "YuHui",
+  //       age: 18
+  //     };
+  //   } else {
+  console.log("session is: ", ctx.session);
+  //   }
+  //   await next();
+  // });
+
   server.use(router.routes());
   // 根据浏览器地址栏请求的 params 来进行相关 query 的配置
   router.get("/a/:id", async ctx => {
     const id = ctx.params.id;
-    await app.render(ctx.req, ctx.res, "/a", { id });
+    // await app.render(ctx.req, ctx.res, "/a", { id });
+    await handle(ctx.req, ctx.res, {
+      pathname: "/a",
+      query: { id }
+    });
     ctx.respond = false;
+  });
+
+  router.get("/set/user", async ctx => {
+    // const id = ctx.params.id;
+    // // await app.render(ctx.req, ctx.res, "/a", { id });
+    // await handle(ctx.req, ctx.res, {
+    //   pathname: "/a",
+    //   query: { id }
+    // });
+    ctx.respond = false;
+    ctx.session.user = {
+      name: "YuHui",
+      age: 18
+    };
+    ctx.body == "set session success";
   });
 
   // 通配符
   router.get("*", async ctx => {
     await handle(ctx.req, ctx.res);
     // hack 手段，兼容 node 底层的 req 和 res
-    ctx.respond = false;
+    ctx.respond = false; // 不再使用内置的
   });
 
   // 使用中间件
   server.use(async (ctx, next) => {
+    ctx.cookies.set("id", "userid_dex", {});
     ctx.res.statusCode = 200;
     await next();
   });
