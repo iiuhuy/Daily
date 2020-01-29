079ffb8cb0ab268bc132b4bc0a3b44b1858e1591
AlvinMi <alvin.mi0620@gmail.com>
2020-01-29 23:35:59 +0800

    ts chapter1

--- TypeScript/01/index.js
+++ TypeScript/01/index.js
@@ -1,11 +1,8 @@
 // let list: number[] = [1, 2, 3, 4];
-
 // 数组泛型
 // let list: Array<number> = [1, 2, 3];
-
 // let x: ["hello", 10];
 // x = ["hello", 10];
-
 // // 枚举 类型
 // enum Color {
 //   Red = 1,
@@ -15,19 +12,9 @@
 // let colorName: string = Color[3];
 // console.log(Color);
 // console.log(colorName);
-
 // any 类型
-
-
 // void 类型
-// function userAccount(): void {
-//   console.log("this is void function");
-// }
-
-// let unusable: void = undefined; // 赋值 undefined 或者 null 都是无意义的
-
-
-// 为什么只能赋值 null 和 undefined ?
-// TypeScript 的 null 和 undefined 类型
-
-// 
+function userAccount() {
+    console.log("this is void function");
+}
+var unusable = false; // 赋值 undefined 无意义
